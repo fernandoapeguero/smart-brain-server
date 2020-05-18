64671ef680d497edfcc29461050b6f299694821a
fernando peguero <fernandoapeguero@gmail.com>
2020-05-18 17:08:06 -0400

    made changes to the server

--- controller/image.js
+++ controller/image.js
@@ -5,7 +5,6 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req , res) =>{
 
-  
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => res.json(data))
         .catch(err => res.status(400).json("unable to work with api"));
